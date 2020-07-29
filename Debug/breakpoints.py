4bb598f02db7d4c315c86665c0e7f21594269067
Al-Bara El-Hag <a.elhag01@gmail.com>
2020-07-29 18:38:56 -0400

    added cool breakpoints

---
+++ Debug/breakpoints.py
@@ -0,0 +1,25 @@
+import ipdb
+
+
+import ipdb
+dbg1 = ipdb.set_trace  # BREAKPOINT
+for i in range(10):
+    my_var2 = 10 / 3
+    dbg1()  # BREAKPOINT
+    dbg1 = lambda: None
+    print(my_var2)
+
+
+dbg2 = ipdb.set_trace  # BREAKPOINT
+for i in range(10):
+    my_var2 = 20 / 3
+    dbg2()  # BREAKPOINT
+    dbg2 = lambda: None
+    print(my_var2)
+
+
+
+# def f(): pass
+# ipdb.set_trace = f
+
+# ipdb.set_trace = lambda: None
--- Debug/main.py
+++ Debug/main.py
@@ -1,7 +1,8 @@
-import pdb
+import ipdb
+
 def main():
   list = [1,2,3]
-  pdb.set_trace() # type `continue` or `c` to step through the code
+  ipdb.set_trace() # type `continue` or `c` to step through the code
   list = [2,3,4]
-if __name__ == '__main__':
-    main()
+
+main()
