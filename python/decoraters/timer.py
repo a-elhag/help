0e5007b1237325fff3156005452d450d5a1bdb96
Al-Baraa El-Hag <a.elhag01@gmail.com>
2020-07-31 20:30:36 -0400

    time decorators

---
+++ python/decoraters/timer.py
@@ -0,0 +1,18 @@
+import time
+
+def timing(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        output = func(*args, **kwargs)
+        end_time = time.time() - start_time
+        print(end_time)
+        return output
+    return wrapper
+
+@timing
+def test():
+    time.sleep(2)
+
+
+test()
+
