15789eaac4b745acd0ee8918ac05b30d53690c6d
Al-Baraa El-Hag <a.elhag01@gmail.com>
2020-08-09 21:49:23 -0400

    select first row and then delete

--- python/sqlite3/numpy/array_class.py
+++ python/sqlite3/numpy/array_class.py
@@ -17,13 +17,13 @@ class SQLNumpy():
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         arr ARRAY);""")
 
-    def adapt_array(arr):
+    def adapt_array(self, arr):
         out = io.BytesIO()
         np.save(out, arr)
         out.seek(0)
         return sqlite3.Binary(out.read())
 
-    def convert_array(text):
+    def convert_array(self, text):
         out = io.BytesIO(text)
         out.seek(0)
         return np.load(out)
@@ -31,8 +31,28 @@ class SQLNumpy():
     def insert(self, arr):
         self.cursor.execute("INSERT INTO data (arr) values (?)", (arr, ))
 
+    def first_select(self):
+        self.cursor.execute("SELECT arr FROM data LIMIT 1")
+        self.data = self.cursor.fetchall()
 
-x = np.arange(12).reshape(2,6)
+    def first_delete(self):
+        self.cursor.execute("""DELETE FROM data WHERE id in (
+            SELECT id FROM data LIMIT 1)""")
+
+    def commit(self):
+        self.conn.commit()
+
+    def close(self):
+        self.conn.close()
+
+x = np.random.rand(8760)
 
 x_sql = SQLNumpy("test.db")
-x_sql.insert(x)
+# x_sql.insert(x)
+
+x_sql.first_select()
+print(x_sql.data)
+x_sql.first_delete()
+x_sql.commit()
+x_sql.close()
+
