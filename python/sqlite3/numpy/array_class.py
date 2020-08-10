f1a8b264d8c8334aa77f2bf5d1f3e562b355886d
Al-Baraa El-Hag <a.elhag01@gmail.com>
2020-08-09 21:18:53 -0400

    creating table

--- python/sqlite3/numpy/array_class.py
+++ python/sqlite3/numpy/array_class.py
@@ -3,6 +3,7 @@ import numpy as np
 import sqlite3
 
 class SQLNumpy():
+    """This will insert a numpy array into a sqlite3 instance"""
     def __init__(self, db_name):
         # Converts np.array to TEXT when inserting
         sqlite3.register_adapter(np.ndarray, self.adapt_array)
@@ -12,6 +13,9 @@ class SQLNumpy():
         self.db_name = db_name
         self.conn = sqlite3.connect(self.db_name, detect_types=sqlite3.PARSE_DECLTYPES)
         self.cursor = self.conn.cursor()
+        self.cursor.execute("""CREATE TABLE IF NOT EXISTS data (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        arr ARRAY);""")
 
     def adapt_array(arr):
         out = io.BytesIO()
@@ -19,10 +23,16 @@ class SQLNumpy():
         out.seek(0)
         return sqlite3.Binary(out.read())
 
-
     def convert_array(text):
         out = io.BytesIO(text)
         out.seek(0)
         return np.load(out)
 
+    def insert(self, arr):
+        self.cursor.execute("INSERT INTO data (arr) values (?)", (arr, ))
 
+
+x = np.arange(12).reshape(2,6)
+
+x_sql = SQLNumpy("test.db")
+x_sql.insert(x)
